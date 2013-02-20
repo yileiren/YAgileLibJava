438c1437e06e02017f69afd5b1dc5cb2f26edfb8
yileiren <yileiren@126.coom>
2013-02-20 17:54:46 +0800

    增加网络数据发送方法。

--- src/ylr/YNetwork/YConnection.java
+++ src/ylr/YNetwork/YConnection.java
@@ -1,5 +1,11 @@
 package ylr.YNetwork;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
  * 网络连接处理类，用来处理连接状态和数据传输。
  * 数据传输过程中数据包格式为：
@@ -8,9 +14,9 @@ package ylr.YNetwork;
  * 
  * 详细说明：
  * 状态字：标识该数据包的类型，状态字类型为StatusWord中的。
- * 数据包总个数：本次发送的数据被分成的数据包总数，无符号整形。
- * 当前数据包序号：本次发送的数据包在总个数中的序号，无符号整形。
- * 数据包中数据长度：本数据包中的数据长度，无符号整形。
+ * 数据包总个数：本次发送的数据被分成的数据包总数，数据类型为int。
+ * 当前数据包序号：本次发送的数据包在总个数中的序号，数据类型为int。
+ * 数据包中数据长度：本数据包中的数据长度，数据类型为int。
  * 数据：数据包中的数据，长度为数据包中数据长度约定的长度。
  * 结束：使用空字符（'\0'）做为结束标识。
  * 
@@ -18,5 +24,223 @@ package ylr.YNetwork;
  * */
 public class YConnection
 {
+	/**
+	 * 网络传输包状态字。
+	 * @author 董帅 创建时间：2013-2-20 14:40:38
+	 *
+	 */
+	public static final class StatusWord
+	{
+		public static final int EndFlag = 0x00;       /**< 数据包结束位。 */
+		
+		public static final int Yes = 0x01;           /**< 请求成功。 */
+		public static final int No = 0x02;            /**< 请求失败。 */
+		public static final int Go = 0x03;            /**< 继续。 */
+		public static final int End = 0x04;           /**< 结束。 */
+		public static final int Error = 0x05;         /**< 出错。 */
+		
+		public static final int SendData = 0x21;      /**< 请求发送数据。 */
 
+		public static final int DataPackage = 0xFF;   /**< 数据包，不含有状态。 */
+	};
+	
+	/**
+	 * 默认构造函数。
+	 */
+	YConnection()
+	{
+		
+	}
+	
+	/**
+	 * 使用指定的套接字发送二进制数据。
+	 * 
+	 * 数据发送握手协议：
+	 * 1、请求发送数据。
+	 * 2、等待服务器响应。
+	 * 3、如果允许发送则逐个包发送数据，每个包发送结束后都要获取服务器响应结果。
+	 * 4、所有数据发送完毕后发送结束协议。
+	 * 5、退出。
+	 * 
+	 * @author 董帅 创建时间：2013-2-20 14:36:03
+	 * 
+	 * @param s 指定的套接字。
+	 * @param data 要发送的数据。
+	 * @param packageLength 被每个数据包最大长度。
+	 * @param time 超时时间。
+	 * 
+	 * @return 成功返回true，否则返回false。
+	 * 
+	 * @throws Exception 程序未处理异常。
+	 */
+	public static boolean sendData(Socket s,byte[] data,int packageLength,int time) throws Exception
+	{
+		boolean retValue = true;
+		
+		try
+		{
+			s.setSoTimeout(time); //设置超时时间。
+			
+			OutputStream socketSndStream = s.getOutputStream(); //获取发送数据流
+			InputStream socketRcvStream = s.getInputStream(); //获取接收数据流
+			
+			//发送数据请求
+			byte[] sendDataReq = new byte[14];
+			sendDataReq[0] = YConnection.StatusWord.SendData; //设置状态字
+			
+			//设置请求数据包总数
+			sendDataReq[4] = (byte)((1 >> 24) & 0xFF);
+			sendDataReq[3] = (byte)((1 >> 16) & 0xFF);
+			sendDataReq[2] = (byte)((1 >> 8) & 0xFF);
+			sendDataReq[1] = (byte)(1 & 0xFF);
+			
+			//设置请求数据包序号
+			sendDataReq[8] = (byte)((1 >> 24) & 0xFF);
+			sendDataReq[7] = (byte)((1 >> 16) & 0xFF);
+			sendDataReq[6] = (byte)((1 >> 8) & 0xFF);
+			sendDataReq[5] = (byte)(1 & 0xFF);
+			
+			//设置数据长度
+			sendDataReq[12] = (byte)((0 >> 24) & 0xFF);
+			sendDataReq[11] = (byte)((0 >> 16) & 0xFF);
+			sendDataReq[10] = (byte)((0 >> 8) & 0xFF);
+			sendDataReq[9] = (byte)(0 & 0xFF);
+			
+			//设置结束位
+			sendDataReq[13] = YConnection.StatusWord.EndFlag;
+			
+			//发送
+			socketSndStream.write(sendDataReq);
+			
+			//接收响应
+			byte[] sendDataRes = new byte[20];
+			int r = socketRcvStream.read(sendDataRes);
+			
+			if(r > 0)
+			{
+				//服务器允许发送数据
+				if(YConnection.StatusWord.Yes == sendDataRes[0])
+				{
+					byte dataBuf[] = new byte[packageLength + 14];//数据发送缓冲区
+					
+					//计算数据包总数
+					int packageCount = data.length / packageLength;
+					if(data.length % packageLength > 0)
+					{
+						packageCount++;
+					}
+					
+					dataBuf[4] = (byte)((packageCount >> 24) & 0xFF);
+					dataBuf[3] = (byte)((packageCount >> 16) & 0xFF);
+					dataBuf[2] = (byte)((packageCount >> 8) & 0xFF);
+					dataBuf[4] = (byte)(packageCount & 0xFF);
+					
+					dataBuf[packageLength + 13] = YConnection.StatusWord.EndFlag;
+				
+					//发送数据
+					int sendLength = 0; //发送数据计数
+					while(data.length - sendLength > 0)
+					{
+						dataBuf[0] = (byte)YConnection.StatusWord.DataPackage; //设置状态字
+						//当前数据包序号
+						int packageOrder = sendLength / packageLength + 1;
+						dataBuf[8] = (byte)((packageOrder >> 24) & 0xFF);
+						dataBuf[7] = (byte)((packageOrder >> 16) & 0xFF);
+						dataBuf[6] = (byte)((packageOrder >> 8) & 0xFF);
+						dataBuf[5] = (byte)(packageOrder & 0xFF);
+						
+						//计算当前数据包中数据长度
+						int l = 0;
+						if(data.length - sendLength >= packageLength)
+						{
+							l = packageLength;
+						}
+						else
+						{
+							l = data.length - sendLength;
+							
+						}
+						
+						dataBuf[12] = (byte)((l >> 24) & 0xFF);
+						dataBuf[11] = (byte)((l >> 16) & 0xFF);
+						dataBuf[10] = (byte)((l >> 8) & 0xFF);
+						dataBuf[9] = (byte)(l & 0xFF);
+						
+						//拷贝数据到dataBuf
+						for(int i = 0;i < l;i++)
+						{
+							dataBuf[13 + i] = data[sendLength + i];
+						}
+						
+						sendLength += l;
+						
+						//发送
+						socketSndStream.write(dataBuf, 0, l);
+						
+						//接收服务器响应
+						r = socketRcvStream.read(dataBuf);
+						if(r > 0)
+						{
+							if(YConnection.StatusWord.Go != dataBuf[0])
+							{
+								//对方不允许继续发送数据
+								retValue = false;
+								break;
+							}
+						}
+						else
+						{
+							//发送数据包后接收对方回复失败
+							retValue = false;
+							break;
+						}
+					}
+				}
+				else
+				{
+					//服务器不允许发送数据
+					retValue = false;
+				}
+			}
+			else
+			{
+				//接收发送请求响应数据失败。
+				retValue = false;
+			}
+			
+			//数据传输结束
+			byte[] sendEndDataReq = new byte[14];
+			sendEndDataReq[0] = YConnection.StatusWord.End; //设置状态字
+			
+			//设置请求数据包总数
+			sendEndDataReq[4] = (byte)((1 >> 24) & 0xFF);
+			sendEndDataReq[3] = (byte)((1 >> 16) & 0xFF);
+			sendEndDataReq[2] = (byte)((1 >> 8) & 0xFF);
+			sendEndDataReq[1] = (byte)(1 & 0xFF);
+			
+			//设置请求数据包序号
+			sendEndDataReq[8] = (byte)((1 >> 24) & 0xFF);
+			sendEndDataReq[7] = (byte)((1 >> 16) & 0xFF);
+			sendEndDataReq[6] = (byte)((1 >> 8) & 0xFF);
+			sendEndDataReq[5] = (byte)(1 & 0xFF);
+			
+			//设置数据长度
+			sendEndDataReq[12] = (byte)((0 >> 24) & 0xFF);
+			sendEndDataReq[11] = (byte)((0 >> 16) & 0xFF);
+			sendEndDataReq[10] = (byte)((0 >> 8) & 0xFF);
+			sendEndDataReq[9] = (byte)(0 & 0xFF);
+			
+			//设置结束位
+			sendEndDataReq[13] = YConnection.StatusWord.EndFlag;
+			
+			socketSndStream.write(sendEndDataReq);
+		}
+		catch(Exception ex)
+		{
+			retValue = false;
+			throw ex;
+		}
+		
+		return retValue;
+	}
 }
--- src/ylr/main/Main.java
+++ src/ylr/main/Main.java
@@ -1,11 +1,104 @@
 package ylr.main;
 
+import java.net.Socket;
+
+import ylr.YNetwork.YConnection;
+
 public class Main {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-
+		while(true)
+		{
+			try
+			{
+				System.out.println("测试程序启动，请选择您要进行的操作：");
+				System.out.println("1、测试网络数据发送。");
+				System.out.println("0、退出程序。");
+				System.out.print("请输入您的选择：");
+				
+				byte[] inputBuf = new byte[200];
+				int inputCount = System.in.read(inputBuf);
+				
+				byte[] inputText = new byte[inputCount - 2];
+				for(int i = 0;i < inputCount  - 2;i++)
+				{
+					inputText[i] = inputBuf[i];
+				}
+				
+				String s = new String(inputText);
+				int inputNum = Integer.valueOf(s);
+				
+				if(inputNum == 0)
+				{
+					System.out.println("程序已退出！");
+					break;
+				}
+				else if(inputNum == 1)
+				{
+					sendData();
+				}
+				else
+				{
+					System.out.println("你输入的序号有误，成重新输入！");
+				}
+			}
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}
 	}
 
+	public static void sendData()
+	{
+		try
+		{
+			System.out.print("请输入服务器IP：");
+			byte[] inputBuf = new byte[2000];
+			int inputCount = System.in.read(inputBuf);
+			
+			byte[] inputText = new byte[inputCount - 2];
+			for(int i = 0;i < inputCount  - 2;i++)
+			{
+				inputText[i] = inputBuf[i];
+			}
+			String ip = new String(inputText);
+			
+			System.out.print("请输入服务器监听端口：");
+			inputCount = System.in.read(inputBuf);
+			
+			inputText = new byte[inputCount - 2];
+			for(int i = 0;i < inputCount  - 2;i++)
+			{
+				inputText[i] = inputBuf[i];
+			}
+			int port = Integer.valueOf(new String(inputText));
+			
+			System.out.print("请输入您要发送的数据：");
+			inputCount = System.in.read(inputBuf);
+			
+			byte[] sendText = new byte[inputCount - 2];
+			for(int i = 0;i < inputCount  - 2;i++)
+			{
+				sendText[i] = inputBuf[i];
+			}
+			
+			String utf8Text = new String(sendText);
+			
+			Socket s = new Socket(ip, port);
+			if(YConnection.sendData(s, utf8Text.getBytes("GBK"), 20, 1000))
+			{
+				System.out.println("发送数据成功！");
+			}
+			else
+			{
+				System.out.println("发送数据失败！");
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 }
